c37e1bec23608fac714194011a41e126365ee7cf
Renan Rocha <legend.oliveira@gmail.com>
2019-01-26 12:01:52 -0200

    Developing spawn system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,4 +10,22 @@ public class GameManager : Singleton<GameManager>
     public List<string> listOfKeys = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "y", "w", "x", "z" };
     // List of removed letters
     public List<string> outKeys = new List<string>();
+
+    public List<Parents> parents;
+
+    public List<string> adjectives;
+
+    public Parents GetParents() {
+        return parents[Random.Range(0, parents.Count - 1)];
+    }
+
+    public string Adjectives() {
+        return adjectives[Random.Range(0, adjectives.Count - 1)];
+    }
+}
+
+[System.Serializable]
+public class Parents {
+    public string name;
+    public Sprite sprite;
 }
